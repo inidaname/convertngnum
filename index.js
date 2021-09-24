f5ba90ffb9c30c3178b3184a42b874f7a508e4dc
Hassan Sani <saniyhassan@gmail.com>
2021-09-24 03:34:03 +0100

    Fix convertNumber not a function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //add +234 to number
 exports.module = convertNumber = (number) => {
+    if (!number) {
+        return
+    }
     const rawNumber = number.substring(number.length - 10, number.length);
     const convNum = `+234${rawNumber}`;
     return convNum;
